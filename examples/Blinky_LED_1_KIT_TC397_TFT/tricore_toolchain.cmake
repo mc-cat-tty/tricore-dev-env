c4e605fe13964b1f51873df973e411ff5e54c21b
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-03-20 18:09:00 +0100

    symbolically link project-template files to Blinky example

--- examples/Blinky_LED_1_KIT_TC397_TFT/tricore_toolchain.cmake
+++ examples/Blinky_LED_1_KIT_TC397_TFT/tricore_toolchain.cmake
@@ -1,5 +1,1 @@
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_SYSTEM_PROCESSOR TC397)
-
-set(CMAKE_C_COMPILER tricore-elf-gcc)
-set(CMAKE_SYSROOT /Users/francesco/Downloads/newlib-tc/install/tricore-elf)
+../../project-template/tricore_toolchain.cmake
