d6ba79f00a2c1cec3048d214f43dff922d819295
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-03-21 12:46:46 +0100

    from link to real file

--- examples/Blinky_LED_1_KIT_TC397_TFT/tricore_toolchain.cmake
+++ examples/Blinky_LED_1_KIT_TC397_TFT/tricore_toolchain.cmake
@@ -1,1 +1,5 @@
-../../project-template/tricore_toolchain.cmake
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR TC397)
+
+set(CMAKE_C_COMPILER tricore-elf-gcc)
+set(CMAKE_SYSROOT /tricore-build-tools/tricore-elf)
