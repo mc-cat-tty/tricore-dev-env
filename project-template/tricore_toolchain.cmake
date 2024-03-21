41afba4c93c71248653437c48e74960d1312d6a3
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-03-21 11:16:05 +0100

    edit sysroot according to containerized environment

--- project-template/tricore_toolchain.cmake
+++ project-template/tricore_toolchain.cmake
@@ -2,4 +2,4 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR TC397)
 
 set(CMAKE_C_COMPILER tricore-elf-gcc)
-set(CMAKE_SYSROOT /Users/francesco/Downloads/newlib-tc/install/tricore-elf)
+set(CMAKE_SYSROOT /tricore-build-tools/tricore-elf)
